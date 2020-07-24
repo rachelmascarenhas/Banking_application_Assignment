a46a8e527f141564f2a843a9bee4b057d3df2c5d
rachelmascarenhas <30861629+rachelmascarenhas@users.noreply.github.com>
2020-07-25 00:45:21 +0530

    Update AccountMainV7.java

--- AccountMainV7.java
+++ AccountMainV7.java
@@ -3,8 +3,8 @@ package com.montran.main;
 import java.util.Scanner;
 
 import com.montran.pojo.Account;
-import com.montran.pojo.Savings;
 import com.montran.pojo.Current;
+import com.montran.pojo.Savings;
 
 public class AccountMainV7 {
 	public static void main(String[] args) {
@@ -20,6 +20,7 @@ public class AccountMainV7 {
 		boolean isSalary = false;
 		boolean result;
 		double overdraftBalance;
+		Account account = null;
 
 		System.out.println(" Hello Welcome to XYZ Bank !!");
 
@@ -29,127 +30,67 @@ public class AccountMainV7 {
 		System.out.println("Enter your choice");
 		accountChoice = scanner.nextInt();
 
+		System.out.println("Enter account Number");
+		accountNumber = scanner.nextInt();
+
+		System.out.println("Enter Name");
+		name = scanner.next();
+
+		System.out.println("Enter Balance");
+		balance = scanner.nextDouble();
+
 		switch (accountChoice) {
 		case 1:
 			System.out.println("Do you want to open salary account (true-false)");
 			isSalary = scanner.nextBoolean();
-			System.out.println("Enter account Number");
-			accountNumber = scanner.nextInt();
-
-			System.out.println("Enter Name");
-			name = scanner.next();
-
-			System.out.println("Enter Salary");
-			balance = scanner.nextDouble();
-
-			Savings savings = new Savings(accountNumber, name, balance, isSalary);
-			System.out.println("Your account is opened successfully !!! ");
-			System.out.println(savings);
-			do {
-				System.out.println("Menu");
-				System.out.println("1. Withdraw");
-				System.out.println("2. Deposit");
-				System.out.println("3. Balance");
-				System.out.println("Enter your choice");
-				trainsactionChoice = scanner.nextInt();
-
-				switch (trainsactionChoice) {
-				case 1:
-					System.out.println("Enter amount to withdraw");
-					amount = scanner.nextInt();
-					result = savings.withdraw(amount);
-					if (result)
-						System.out.println("Transaction Success !!");
-					else
-						System.out.println("Transaction Failed !!");
-					break;
-				case 2:
-					System.out.println("Enter amount to deposit");
-					amount = scanner.nextInt();
-					result = savings.deposit(amount);
-					if (result)
-						System.out.println("Transaction Success !!");
-					else
-						System.out.println("Transaction Failed !!");
-					break;
-				case 3:
-					System.out.println("Account Balance :: " + savings.getBalance());
-					break;
-				default:
-					System.out.println("Invalid choice");
-					break;
-				}
-
-				System.out.println("Do you want to continue ?");
-				continueChoice = scanner.next();
-			} while (continueChoice.equals("yes"));
-			break;
+			account = new Savings(accountNumber, name, balance, isSalary);   //using polymorphism 
+			break; 
 		case 2:
-			
-			System.out.println("Enter account Number");
-			accountNumber = scanner.nextInt();
-
-			System.out.println("Enter Name");
-			name = scanner.next();
-
-			System.out.println("Enter Balance");
-			balance = scanner.nextDouble();
-			
-			System.out.println("Enter overdraft balance");
-			overdraftBalance=scanner.nextDouble();
-			Current current=new Current(accountNumber,name,balance,overdraftBalance);
-			System.out.println("Your account is opened successfully !!! ");
-			System.out.println(current);
-			do {
-				System.out.println("Menu");
-				System.out.println("1. Withdraw");
-				System.out.println("2. Deposit");
-				System.out.println("3. Balance");
-				System.out.println("Enter your choice");
-				trainsactionChoice = scanner.nextInt();
-
-				switch (trainsactionChoice) {
-				case 1:
-					System.out.println("Enter amount to withdraw");
-					amount = scanner.nextInt();
-					result = current.withdraw(amount);
-					if (result)
-						System.out.println("Transaction Success !!");
-					else
-						System.out.println("Transaction Failed !!");
-					break;
-				case 2:
-					System.out.println("Enter amount to deposit");
-					amount = scanner.nextInt();
-					result = current.deposit(amount);
-					if (result)
-						System.out.println("Transaction Success !!");
-					else
-						System.out.println("Transaction Failed !!");
-					break;
-				case 3:
-					System.out.println("Account Balance :: " + current.getBalance());
-					System.out.println("Overdraft Balance::"+ current.getOverdradtBalance());
-					break;
-				default:
-					System.out.println("Invalid choice");
-					break;
-				}
-
-				System.out.println("Do you want to continue ?");
-				continueChoice = scanner.next();
-			} while (continueChoice.equals("yes"));
+			System.out.println("Enter Overdraft Balance");
+			overdraftBalance = scanner.nextDouble();
+			account = new Current(accountNumber, name, balance, overdraftBalance);   //using polymorphism
 		default:
-			System.out.println("Thank you for banking with us ");
 			break;
 		}
 
-		
-		
+		do {
+			System.out.println("Menu");
+			System.out.println("1. Withdraw");
+			System.out.println("2. Deposit");
+			System.out.println("3. Balance");
+			System.out.println("Enter your choice");
+			trainsactionChoice = scanner.nextInt();
 
-		
+			switch (trainsactionChoice) {
+			case 1:
+				System.out.println("Enter amount to withdraw");
+				amount = scanner.nextInt();
+				result = account.withdraw(amount);
+				if (result)
+					System.out.println("Transaction Success !!");
+				else
+					System.out.println("Transaction Failed !!");
+				break;
+			case 2:
+				System.out.println("Enter amount to deposit");
+				amount = scanner.nextInt();
+				result = account.deposit(amount);
+				if (result)
+					System.out.println("Transaction Success !!");
+				else
+					System.out.println("Transaction Failed !!");
+				break;
+			case 3:
+				System.out.println("Account Balance :: " + account.getBalance());
+				break;
+			default:
+				System.out.println("Invalid choice");
+				break;
+			}
+
+			System.out.println("Do you want to continue ?");
+			continueChoice = scanner.next();
+		} while (continueChoice.equals("yes"));
 		scanner.close();
-
 	}
 }
-
